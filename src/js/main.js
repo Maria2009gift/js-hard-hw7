bfd6fb33187fbc03994b3578d0bd2da33a0bba16
Maria2009gift <kovalenkom2009@gmail.com>
2024-03-18 23:12:43 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+import students from "./students.json";
+import menuTemplate from '../templates/menu.hbs';
+import "../style.css";
+
+const container = document.querySelector(".container")
+const btnFilterAge = document.querySelector("#btnFilterAge")
+const btnFilterGrade = document.querySelector("#btnFilterGrade")
+
+
+const noFilterStudent = menuTemplate(students)
+container.insertAdjacentHTML('beforeend', noFilterStudent);
+
+btnFilterAge.addEventListener("click", fFilterAge)
+btnFilterGrade.addEventListener("click", fFilterGrade)
+
+function fFilterAge() {
+    container.innerHTML = ""
+
+    const ageFilteredSudents = students.sort((firstStudent, secondStudent) => firstStudent.age - secondStudent.age)
+    const menuAgeFilteredSudents = menuTemplate(ageFilteredSudents)
+    container.insertAdjacentHTML('beforeend', menuAgeFilteredSudents);
+}
+
+function fFilterGrade() {
+    container.innerHTML = ""
+    const gradeFilteredSudents = students.sort((firstStudent, secondStudent) => firstStudent.grade - secondStudent.grade)
+    const menuAgeFilteredSudents = menuTemplate(gradeFilteredSudents)
+    container.insertAdjacentHTML('beforeend', menuAgeFilteredSudents);
+}
+
+
+
+
+
+
+
